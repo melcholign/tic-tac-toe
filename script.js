075c9c84b2c7aafc962c5684ffce134d02e4d884
melcholign <115886450+melcholign@users.noreply.github.com>
2024-02-27 22:38:40 +0600

    Form the outline of the Gameboard factory
    Its methods are not implemented yet. Only the interface to them are
    defined.

---
+++ script.js
@@ -0,0 +1,22 @@
+function Gameboard(boardLength) {
+
+    let board = [];
+
+    // builds up the board
+    for (let row = 0; row < boardLength; ++row) {
+        board[row] = [];
+        for (let col = 0; col < boardLength; ++col) {
+            board[row][col] = null;     // null is a placeholder for a cell object 
+        }
+    }
+
+    function getCellMark(cellRow, cellCol) {
+        
+    }
+
+    function markCell(mark, cellRow, cellCol) {
+
+    }
+
+    return { getCellMark, markCell };
+}
