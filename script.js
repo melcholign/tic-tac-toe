bdaa601b1bb252e0e393fd0ffcb3d46f813b4005
melcholign <115886450+melcholign@users.noreply.github.com>
2024-03-13 07:54:29 +0600

    Implement end-game conditions
    The win-condition check if the cells are the same as the recently marked
    cell in the same row, column, or any of the two axial diagonals of
    the board.
    
    The implementation of the win condition required the use of a new method
    (isHomogeneousCellGroup) of the Gameboard object that returns whether or
    not all the cells in a group contains the same mark. A group contains
    cells in a line from one boundary to its opposite of the board. The
    orientation of the line is determined by passing callback functions
    as the coordinates of the cell that may return a constant value or a
    linear relationship.
    
    The tie condition merely determines whether all cells have been marked.
    However, it is not explicitly implemented by the code, as the code only
    checks if the round number is the same as the size of the board. This
    makes sense because after that number of rounds, the players will
    have no more cells to mark. Hence, assuming that no one has won, the tie
    condition evaluates to true.
    
    The current limitations to the code include:
        - the win and tie conditions are decoupled and accessible by
          external code. This means that the responsibility to test them
          in the correct order lies on the user of their respective methods.
    
        - as a corollary to the last point, the parameters of the win method
          that must correspond to the cell coordinates of the recently-added
          mark necessitates the user to maintain consistency between the two
          in order for the end-game logic to function per requirements.
    
        - The tie condition is inefficient as it is not able to predict
          its outcome until board is completely filled.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function Gameboard(boardLength = 3) {
+function Gameboard(boardSize = 3) {
 
     // builds up the board
     let board = [];
-    for (let row = 0; row < boardLength; ++row) {
+    for (let row = 0; row < boardSize; ++row) {
         board[row] = [];
-        for (let col = 0; col < boardLength; ++col) {
+        for (let col = 0; col < boardSize; ++col) {
             board[row][col] = Cell();
         }
     }
@@ -14,7 +14,7 @@ function Gameboard(boardLength = 3) {
     }
 
     function markCell(mark, cellRow, cellCol) {
-        if (cellRow <= boardLength && cellCol <= boardLength) {
+        if (cellRow <= boardSize && cellCol <= boardSize) {
             board[cellRow][cellCol].setMark(mark);
         }
     }
@@ -23,7 +23,15 @@ function Gameboard(boardLength = 3) {
         board.forEach(row => console.log(row.map(cell => cell.getMark() || '_')));
     }
 
-    return { getCellMark, markCell, printBoard };
+    // a cell group contains exactly boardSize number of cells in a line
+    // it is homogeneous when all its cells have the same mark
+    function isHomogeneousCellGroup(mark, onCellRow, onCellCol) {
+        let i;
+        for (i = 0; i < boardSize && board[onCellRow(i)][onCellCol(i)].getMark() === mark; ++i);
+        return i === boardSize;
+    }
+
+    return { getCellMark, markCell, printBoard, isHomogeneousCellGroup };
 }
 
 function Cell() {
@@ -67,7 +75,9 @@ function GameController(
     playerTwo = Player('P2', 'O')
 ) {
 
-    const board = Gameboard();
+    const boardSize = 3;
+    const board = Gameboard(boardSize);
+
     let activePlayer = playerOne;
     let roundNumber = 1;
 
@@ -86,9 +96,22 @@ function GameController(
         ++roundNumber;
     }
 
+    function checkWin(cellRow, cellCol) {
+        const playerMark = activePlayer.getMark();
+        // checks for homogeneity of the following groups
+        return board.isHomogeneousCellGroup(playerMark, () => cellRow, col => col)      // row
+            || board.isHomogeneousCellGroup(playerMark, row => row, () => cellCol)      // column
+            || (cellRow === cellCol) && board.isHomogeneousCellGroup(playerMark, row => row, col => col)                                // main-axis diagonal
+            || (cellRow === boardSize - cellCol) && board.isHomogeneousCellGroup(playerMark, row => row, col => boardSize - col - 1);   // cross-axis diagonal 
+    }
+
+    function checkTie() {
+        return roundNumber === (boardSize * boardSize);
+    }
+
     function getBoard() {
         return board;
     }
 
-    return { playRound, getBoard };
+    return { playRound, getBoard, checkWin, checkTie };
 }
